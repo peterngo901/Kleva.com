404462cb194f267d89c7c0dee3df1df201745682
peterngo901 <69839909+peterngo901@users.noreply.github.com>
2020-08-31 07:59:11 +1000

    Updated to work with placeholder.

--- models/game.js
+++ models/game.js
@@ -1,3 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'games.json'
+);
+
+const getGamesFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      } else {
+        cb(JSON.parse(fileContent));
+      }
+    });
+  };
+
 module.exports = class Game {
     constructor(title, imageURL, price, description) {
         this.title = title;
@@ -8,6 +27,13 @@ module.exports = class Game {
 
     upload() {
         // Database upload functionality.
+    this.id = Math.random().toString();
+    getGamesFromFile(games => {
+      games.push(this);
+      fs.writeFile(p, JSON.stringify(games), err => {
+        console.log(err);
+      });
+    });
     }
 
     // static fetchAll(callback) { // fetchAll has a function as a parameter which it will execute when it finishes.
@@ -15,11 +41,16 @@ module.exports = class Game {
     // }
 
     static fetchAll(callback) {
-        getGamesFromFile(cb);
+        getGamesFromFile(callback);
       }
 
     static retrieveByID(id, callback) {
         // Database read functionality.
+        getGamesFromFile(games => {
+            const game = games.find(p => p.id === id);
+            cb(games);
+        });
+        
     }
 
 }
