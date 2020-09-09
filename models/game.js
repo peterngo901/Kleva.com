296246b7150c8a44ccc50c44a7db3de415cf0578
peterngo901 <69839909+peterngo901@users.noreply.github.com>
2020-09-09 11:25:26 +1000

    CRUD functionality and UUID

--- models/game.js
+++ models/game.js
@@ -1,21 +1,7 @@
-const fs = require('fs');
-const path = require('path');
+const db = require('../data/database');
+const { v4: uuidv4 } = require('uuid');
 
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  'data',
-  'games.json'
-);
 
-const getGamesFromFile = cb => {
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      } else {
-        cb(JSON.parse(fileContent));
-      }
-    });
-  };
 
 module.exports = class Game {
     constructor(title, imageURL, price, description) {
@@ -25,32 +11,29 @@ module.exports = class Game {
         this.description = description;
     }
 
-    upload() {
-        // Database upload functionality.
-    this.id = Math.random().toString();
-    getGamesFromFile(games => {
-      games.push(this);
-      fs.writeFile(p, JSON.stringify(games), err => {
-        console.log(err);
-      });
-    });
+    // Upload a game for review.
+    create(gameObject) {
+      const title = gameObject.title
+      const description = gameObject.description
+      const id = uuidv4();
+      const values = [title, id, 'https://cdn.factorio.com/assets/img/blog/fff-310-factorio-cover-017-stable-squared.png', 4.99, description]
+      const text = 'INSERT INTO "Game".test(title, id, "imageURL", price, description) VALUES ($1, $2, $3, $4, $5)'
+      db.query(text, values).catch(err => console.log(err));
+
     }
 
-    // static fetchAll(callback) { // fetchAll has a function as a parameter which it will execute when it finishes.
-    //     // Database fetches all games on the store.
-    // }
+    static deleteByID(id) {
 
-    static fetchAll(callback) {
-        getGamesFromFile(callback);
-      }
+    }
 
-    static retrieveByID(id, callback) {
-        // Database read functionality.
-        getGamesFromFile(games => {
-            const game = games.find(p => p.id === id);
-            cb(games);
-        });
-        
+    // List All Games
+    static listAll() {
+      const games  = db.query('SELECT * FROM "Game".test')
+      return games
+    }
+
+    static getByID(id) {
+      
     }
 
 }
