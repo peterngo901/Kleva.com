edb5a63314396c269f9aadaca14b6763e8ea6c52
peterngo901 <spf9237@gmail.com>
2020-09-23 13:48:42 +1000

    Student Single Sign On

--- data/database.js
+++ data/database.js
@@ -1,14 +1,14 @@
-const { Pool } = require('pg')
+// const { Pool } = require('pg')
 
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    database: 'postgres',
-    password: 'royalelephants'
-})
+// const pool = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'postgres',
+//     password: 'royalelephants'
+// })
 
-module.exports = pool;
+// module.exports = pool;
 
 const Sequelize = require('sequelize');
 
