dac6c59bcb65505dfb1d06bdb71dc292b1bc545f
Gabriel McHugh <gabriel.t.mchugh@gmail.com>
2020-09-28 15:50:42 +1000

    merging'

--- data/database.js
+++ data/database.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 // const { Pool } = require('pg')
 
 // const pool = new Pool({
@@ -12,7 +10,6 @@
 
 // module.exports = pool;
 
->>>>>>> 4fa02afae09d99680b2aa3a080c1daf137222d9f
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('Kleva', 'gabriel', '', {
