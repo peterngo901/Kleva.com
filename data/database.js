6d2c0e7cabac81a317764fc6ea3e9c265893741c
Gabriel McHugh <gabriel.t.mchugh@gmail.com>
2020-09-28 15:08:13 +1000

    merging

--- data/database.js
+++ data/database.js
@@ -1,18 +1,6 @@
-const { Pool } = require('pg')
-
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    database: 'postgres',
-    password: 'royalelephants'
-})
-
-module.exports = pool;
-
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('postgres', 'postgres', 'royalelephants', {
+const sequelize = new Sequelize('Kleva', 'gabriel', '', {
     dialect: 'postgres',
     host: 'localhost'
 });
