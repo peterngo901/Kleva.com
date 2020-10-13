a1d6d75f2a434f0c686639ba263c134521a9a785
peterngo901 <spf9237@gmail.com>
2020-10-13 17:04:47 +1000

    Dynamic Doodle Modal

--- routes/gameroom.js
+++ routes/gameroom.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const gameroomController = require('../controllers/gameroom');
+const questionController = require('../controllers/questions');
 
 const router = express.Router();
 
@@ -9,5 +10,8 @@ router.get('/game-room', gameroomController.getGameroom);
 router.get('/teacher/game-room', gameroomController.getTeacherGameroom);
 
 router.post('/teacher/game-room', gameroomController.postTeacherGameroom);
+
+// Classroom Modal Post Request to Database for ACARA Curriculum
+router.post('/game-staging-area', questionController.postYearLevelSubstrand);
 
 module.exports = router;
