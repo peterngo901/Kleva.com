9002f3be62c2a8772462a331127c700dba705b74
peterngo901 <spf9237@gmail.com>
2020-10-14 10:37:49 +1000

    Fixed GameID Foreign Key Error

--- models/classroomStats.js
+++ models/classroomStats.js
@@ -3,17 +3,19 @@ const Sequelize = require('sequelize');
 const sequelize = require('../data/database');
 
 const ClassroomStats = sequelize.define('classroomStats', {
-  gameID: { //gameID and classroomClassCode make a composite primary key
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    allowNull: false,
-  },
-  classroomClassCode: { //gameID and classroomClassCode make a composite primary key
+  gameID: {
+    //gameID and classroomClassCode make a composite primary key
     type: Sequelize.STRING,
     primaryKey: true,
     allowNull: false,
   },
-    AverageStudentActivity: {
+  classroomClassCode: {
+    //gameID and classroomClassCode make a composite primary key
+    type: Sequelize.STRING,
+    primaryKey: true,
+    allowNull: false,
+  },
+  AverageStudentActivity: {
     type: Sequelize.INTEGER,
   },
 });
